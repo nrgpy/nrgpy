089fb9925c84bdfd4ac9756e0caec6ab00af783b
tzotsi <drc@nrgsystems.com>
2018-10-12 14:18:07 -0400

    changed version to 0.1.0, url to blank (will point to pypi.org when branched to there), author to NRG Systems, Inc.

--- nrgpy/setup.py
+++ nrgpy/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='nrgpy',
-      version='0.1',
-      description='library for handling NRG SymphoniePRO Data Logger files',
-      url='https://tzotsi@bitbucket.org/tzotsi/nrgpy.git',
-      author='NRG Technical Services',
+      version='0.1.0',
+      description='library for handling NRG Systems data files',
+      url='',
+      author='NRG Systems, Inc.',
       author_email='support@nrgsystems.com',
       license='MIT',
       keywords='nrg systems rld symphonie symphoniepro',
