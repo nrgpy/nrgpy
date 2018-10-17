d41c0850afa42bcba47793ba40214087f1d59972
tzotsi <drc@nrgsystems.com>
2018-10-17 14:42:36 -0400

    added token to nrg_api_url.py. This can be ignored, or used in order to omit token from command calls

--- nrgpy/nrgpy/nrg_api_url.py
+++ nrgpy/nrgpy/nrg_api_url.py
@@ -1,1 +1,9 @@
+"""
+NRG Convert API location
+    The token variable below will be used for nrg_convert_api calls if populated. 
+    Otherwise, the token will have to be provided each time the nrg_convert_api class
+    is called.
+"""
+
 nrgApiUrl='https://nrgconvert.azurewebsites.net/api/Convert?code=yafm/4r/axuaMMGTP9SkBRNrpmEhrrM4B4sU6ehrXDG6bJaMpFhbIg=='
+token=''
