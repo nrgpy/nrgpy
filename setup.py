1bfb8cab581617e193e2feeb4746bc6d929acb3a
tzotsi <drc@nrgsystems.com>
2018-11-27 11:08:46 -0500

    reverted convert_rld, rest same

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='nrgpy',
-      version='0.1.5',
+      version='0.1.6',
       description='library for handling NRG Systems data files',
       long_description=long_description,
       url='https://github.com/nrgpy/nrgpy',
