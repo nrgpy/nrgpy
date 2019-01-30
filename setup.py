b34e551e9a8a4adfff893dae2731253872d97eaf
David Carlson <drc@nrgsystems.com>
2019-01-29 19:32:44 -0500

    changed version in setup for wheel creation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='nrgpy',
-      version='0.1.8',
+      version='0.1.9',
       description='library for handling NRG Systems data files',
       long_description=long_description,
       url='https://github.com/nrgpy/nrgpy',
