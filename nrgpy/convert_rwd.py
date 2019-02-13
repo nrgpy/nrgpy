98e3486f7d5c94465ac407a13cc5605f37b0e50d
tzotsi <drc@nrgsystems.com>
2019-02-13 11:28:19 -0500

    added convert_rwd.local.check_sdr

--- nrgpy/convert_rwd.py
+++ nrgpy/convert_rwd.py
@@ -12,21 +12,28 @@ from nrgpy.utilities import check_platform, windows_folder_path, linux_folder_pa
 
 class local(object):
     """
-    nrgpy.convert_rwd.local - use local installation of Symphonie Data Retriever to
-    convert *.RWD files to *.TXT
+    nrgpy.convert_rwd.local - use local installation of Symphonie Data Retriever (SDR)
+    to convert *.RWD files to *.TXT
 
     parameters -
+              filename : '', if populated, a single file is exported
         encryption_pin : '', four digit pin, only used for encrypted files
               sdr_path : r'"C:/NRG/SymDR/SDR.exe"', may be any path
            site_filter : '', filters files on text in filename
                rwd_dir : '', folder to check for RWD files
+               out_dir : '', folder to save exported TXT files into
+
+
+    functions -
+          check_sdr : checks operating system for SDR installation
+        export_file :
 
     """
 
 
-    def __init__(self, rwd_dir='', out_dir='', encryption_pass='',
+    def __init__(self, rwd_dir='', out_dir='', filename='', encryption_pass='',
                  sdr_path=r'"C:/NRG/SymDR/SDR.exe"',
-                 convert_type='meas', site_filter='', out_dir='', **kwargs):
+                 convert_type='meas', site_filter='', **kwargs):
         self.encryption_pin = encryption_pin
         self.sdr_path = sdr_path
         self.site_filter = site_filter
@@ -59,15 +66,21 @@ class local(object):
         else:
             # do the linux check
             try:
-                pass
+                subprocess.check_output(['wine','--version'])
+            except NotADirectoryError:
+                print('System not configured for running SDR.\n Please follow instructions in SDR_Linux_README.md to enable.')
+                break
+            try:
+                subprocess.check_output(['wine',self.sdr_path,'/s','test.rwd'])
+                print('SDR test OK!')
             except:
-                pass
-        
-        
-    def check_wine(self):
-        """
-        determine if Wine is installed. Only used if self.platform == 'linux'
-        """
-        pass
+                print('SDR unable to start')
+                break
 
     
+    def export_file(self, filename):
+        # clear RawData and ScaledData folders
+        # copy RWD file to RawData folder
+        # convert the file
+        # copy exported file from ScaledData to self.out_dir
+  
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='nrgpy',
-      version='0.1.13',
+      version='0.2.1',
       description='library for handling NRG Systems data files',
       long_description=long_description,
       long_description_content_type='text/markdown',
