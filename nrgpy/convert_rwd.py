8485487b86bc43328122c8d2bb25b5cef78137f4
tzotsi <drc@nrgsystems.com>
2019-02-13 19:48:46 -0500

    removed unnecessary imports

--- nrgpy/convert_rwd.py
+++ nrgpy/convert_rwd.py
@@ -1,11 +1,7 @@
 #!/bin/usr/python
 
-import io
-import glob
 import os
 import pandas as pd
-from pathlib import Path
-import requests
 import subprocess
 from nrgpy.utilities import check_platform, windows_folder_path, linux_folder_path
 
