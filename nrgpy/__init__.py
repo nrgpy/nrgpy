e45a32e3c22708e53551bbdbee325648ecb79b61
Dave <davec.vt@gmail.com>
2019-04-04 12:00:08 -0400

    fixes to rwd file handling

--- nrgpy/__init__.py
+++ nrgpy/__init__.py
@@ -5,5 +5,5 @@ from .convert_rld import local, nrg_convert_api
 from .convert_rwd import local
 from .ipk2lgr import ipk2lgr
 from .sympro_txt import sympro_txt_read, shift_timestamps
-from .txt_util import data_file_read
+from .txt_utils import data_file_read
 from .utilities import check_platform, windows_folder_path, linux_folder_path
