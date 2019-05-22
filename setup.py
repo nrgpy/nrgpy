3170ca2a4936be0c7bd9f4fb2f9d45be2304334b
Dave <davec.vt@gmail.com>
2019-05-22 16:37:08 -0400

    adding nrgmodbus package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='nrgpy',
-      version='0.3.0',
+      version='0.4.0',
       description='library for handling NRG Systems data files',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -18,6 +18,7 @@ setup(
             'nrgpy'
       ],
       install_requires=[
+            'nrgmodbus',
             'pandas>=0.23',
             'requests',
       ],
