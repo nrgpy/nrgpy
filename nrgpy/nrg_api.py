5a0f3a24c03464a3811089912284daa9d00fa093
xianbei <Dave C>
2020-10-29 11:16:06 -0400

    updated doc

--- nrgpy/nrg_api.py
+++ nrgpy/nrg_api.py
@@ -58,12 +58,12 @@ class nrg_api(object):
         request_token_header = {'content-type': 'application/json'}
         request_payload = {'client_id': '{}'.format(self.client_id), 'client_secret': '{}'.format(self.client_secret)}
 
-        resp = requests.post(data=json.dumps(request_payload), headers=request_token_header, url=retrieve_token_url)
+        self.resp = requests.post(data=json.dumps(request_payload), headers=request_token_header, url=retrieve_token_url)
         self.session_start_time = datetime.now()
 
-        if resp.status_code == 200:
+        if self.resp.status_code == 200:
             print("[OK]")
-            self.session_token = json.loads(resp.text)['access_token']
+            self.session_token = json.loads(self.resp.text)['access_token']
         else:
             print("[FAILED] | unable to get session token.")
             self.session_token = False
@@ -183,16 +183,16 @@ class nrg_api_catalog(nrg_api):
             'enddate': self.end_date,
          }
 
-        resp = requests.post(data=self.data, url=data_catalog_url, headers=self.headers)
+        self.resp = requests.post(data=self.data, url=data_catalog_url, headers=self.headers)
 
-        if resp.status_code == 200:
-            self.f = json.loads(resp.content)
+        if self.resp.status_code == 200:
+            self.f = json.loads(self.resp.content)
             self.json = json.dumps(self.f, indent=2, sort_keys=True)
             self.df = pd.read_json(self.json)
 
         else:
-            print(resp.status_code)
-            print(resp.reason)
+            print(self.resp.status_code)
+            print(self.resp.reason)
             return False
 
 
@@ -486,6 +486,8 @@ class nrg_api_export(nrg_api):
         (True) whether to save the result to file
     nec_file : str, optional
         path to NEC file for custom export formatting
+    text_timestamps : bool
+        get export data with text timestamps instead of datetime
 
     Returns
     -------
@@ -564,11 +566,11 @@ class nrg_api_export(nrg_api):
             'necfilebytes': self.encoded_nec_bytes
         }
 
-        resp = requests.post(data=self.data, url=export_url, headers=self.headers)
+        self.resp = requests.post(data=self.data, url=export_url, headers=self.headers)
 
-        if resp.status_code == 200:
+        if self.resp.status_code == 200:
             with open(self.filepath, 'wb') as f:
-                f.write(resp.content)
+                f.write(self.resp.content)
 
             with zipfile.ZipFile(self.filepath, 'r') as z:
                 data_file = z.namelist()[0]
@@ -586,6 +588,6 @@ class nrg_api_export(nrg_api):
             return reader
 
         else:
-            print(resp.status_code)
-            print(resp.reason)
+            print(self.resp.status_code)
+            print(self.resp.reason)
             return False
