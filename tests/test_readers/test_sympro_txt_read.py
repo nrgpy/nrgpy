f89dc01ab4c24a12d3baf726e7b75f69fb3b121f
David Carlson <drc@nrgsystems.com>
2022-03-18 12:02:08 -0400

    updated directory path for test files... again! x 2

--- tests/test_readers/test_sympro_txt_read.py
+++ tests/test_readers/test_sympro_txt_read.py
@@ -33,4 +33,5 @@ def test_sympro_txt_read(directory="tests/test_readers/test_files"):
 
 if __name__ == "__main__":
 
-    assert test_sympro_txt_read()
+    directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_files")
+    assert test_sympro_txt_read(directory)
