96b7feddac77fa5cac7ea67cf749e900164ff741
David Carlson <drc@nrgsystems.com>
2022-03-23 17:13:46 -0400

    more test troubleshooting...

--- tests/nrgcloud_tests/api_tests.py
+++ tests/nrgcloud_tests/api_tests.py
@@ -5,19 +5,27 @@ except:
 import nrgpy
 import traceback
 
+
 def test_sites_api(client_id, client_secret):
     """Ensure NRG Cloud Sites API is working properly with test account
-    
+
     Test account has access to site 4310, associated with logger 820604310
     """
     try:
         print(f"client_id = {client_id[:4]}...etc.")
-        sites = nrgpy.cloud_sites(
-            client_id,
-            client_secret
-        )
+        sites = nrgpy.cloud_sites(client_id, client_secret)
+        print(f"Sites API hit")
+        try:
+            print(sites.sites_df)
+        except:
+            print("Unable to print sites_df")
 
-        if sites.sites_df['siteId'].loc[sites.sites_df['loggerSerialNumber'] == 820604310][0] != 95:
+        if (
+            sites.sites_df["siteId"].loc[
+                sites.sites_df["loggerSerialNumber"] == 820604310
+            ][0]
+            != 95
+        ):
             print(f"Site ID != 95: SiteIds = {sites.sites_df['siteId']}")
             logger.error(f"Site ID != 95: SiteIds = {sites.sites_df['siteId']}")
             return False
@@ -41,25 +49,25 @@ def test_export_api(client_id, client_secret):
             client_id=client_id,
             client_secret=client_secret,
             site_id=95,
-            start_date='2022-02-22',
-            end_date='2022-02-24',
-            out_dir='.'
+            start_date="2022-02-22",
+            end_date="2022-02-24",
+            out_dir=".",
         )
 
-        reader = nrgpy.sympro_txt_read(
-            filename=exporter.export_filepath
-        )
+        reader = nrgpy.sympro_txt_read(filename=exporter.export_filepath)
 
         if int(reader.site_number) != 4310:
             print(f"Export API test failed: site number failure: {reader.site_number}")
-            logger.error(f"Export API test failed: site number failure: {reader.site_number}")
+            logger.error(
+                f"Export API test failed: site number failure: {reader.site_number}"
+            )
             return False
 
     except:
         print(traceback.format_exc())
         logger.error(traceback.format_exc())
         return False
-    
+
     print("Export API test passed!")
     logger.info("Export API test passed!")
     return True
@@ -67,8 +75,9 @@ def test_export_api(client_id, client_secret):
 
 if __name__ == "__main__":
     import sys
+
     client_id = sys.argv[1]
     client_secret = sys.argv[2]
 
     assert test_sites_api(client_id, client_secret)
-    assert test_export_api(client_id, client_secret)        
+    assert test_export_api(client_id, client_secret)
