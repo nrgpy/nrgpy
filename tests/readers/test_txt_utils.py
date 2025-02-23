cb93a40751a8655caa70809e05462ade9a9da166
Devon Watt <devon.watt@mail.mcgill.ca>
2025-02-23 10:12:13 -0600

    add test

--- tests/readers/test_txt_utils.py
+++ tests/readers/test_txt_utils.py
@@ -6,5 +6,7 @@ TEST_DATA = files('tests') / 'readers/files'
 def test_read_text_data():
     test_file = TEST_DATA / '004310_2022-03-17_00.00_000835_meas.txt'
     reader = read_text_data(filename=test_file, data_type='symphoniepro')
-    assert reader.data is not None
-    assert reader.site_info is not None
+    assert reader.data.shape == (370, 64)
+    assert reader.site_info.shape == (217, 2)
+    assert reader.ch_info.shape == (15, 12)
+    assert reader.ch_info.loc[5, 'Serial Number:'] == '939600122'
